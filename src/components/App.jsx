06ba08f7b0267631df4da0c49de46db6e310eb77
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-26 15:29:41 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 // import tabsData from "../db/tabs.json";
 import { Route, Routes } from "react-router-dom";
 import DummyTable from "./dummyTable";
+import { SharedLayout } from "./SharedLayout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route index component={<DummyTable />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="/dummyTable" component={<DummyTable />} />
+        </Route>
       </Routes>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="backendless_task">
+    <BrowserRouter basename="/backendless_task">
       <App />
     </BrowserRouter>
   </React.StrictMode>
