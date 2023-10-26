6f0b8ac3e8c3626b717d9e3bbea46b3abf9363bd
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-26 12:37:07 +0300

    add

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Tab } from "./Tab";
+import tabsData from "../db/tabs.json";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/components/Tab.jsx
@@ -0,0 +1,7 @@
+export const Tab = ({ content }) => {
+  return (
+    <>
+      <div>{content}</div>
+    </>
+  );
+};
