4e013dff08c5b63749b3b991b5659245c5933cae
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-26 16:30:14 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,48 @@
-// import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { NavLink, Route, Routes, useParams } from "react-router-dom";
 import tabsData from "../db/tabs.json";
-import { NavLink, Route, Routes } from "react-router-dom";
-// import DummyTable from "./dummyTable";
-// import { SharedLayout } from "./SharedLayout";
 
 function App() {
-  // const [tabs, setTabs] = useState(tabsData);
+  const [tabs, setTabs] = useState([]);
+
+  useEffect(() => {
+    // Simulate fetching tab data from the JSON file
+    setTabs(tabsData);
+  }, []);
+
+  function DefaultTabLoader() {
+    const defaultTab = tabs.find((tab) => tab.order === 0);
+    return defaultTab ? <TabContentLoader tab={defaultTab} /> : null;
+  }
+
+  function TabContentLoader() {
+    const { tabId } = useParams();
+    const tab = tabs.find((t) => t.id === tabId);
+
+    if (tab) {
+      return (
+        // Load the tab content lazily using dynamic import
+        <React.Suspense fallback={<div>Loading...</div>}>
+          {React.lazy(() => import(`./${tab.path}`))}
+        </React.Suspense>
+      );
+    } else {
+      return <div>Tab not found</div>;
+    }
+  }
 
   return (
     <>
       <nav>
-        {tabsData.map((tab) => (
+        {tabs.map((tab) => (
           <NavLink key={tab.id} to={tab.id}>
             {tab.title}
           </NavLink>
         ))}
       </nav>
       <Routes>
-        {/* <Route path="/:tabId" element={<TabContentLoader />} />
-        <Route path="/" element={<DefaultTabLoader />} /> */}
+        <Route path="/:tabId" element={<TabContentLoader />} />
+        <Route path="/" element={<DefaultTabLoader />} />
       </Routes>
     </>
   );
