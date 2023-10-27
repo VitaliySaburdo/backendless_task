4c55d89fe5f1f5bf332cbc876a1a2209f15ff034
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-27 16:36:24 +0300

    add

--- src/components/tabs/dummyTable.js
+++ src/components/tabs/dummyTable.js
@@ -1,7 +1,22 @@
+import React from "react";
+
 const DummyTable = () => {
   return (
     <>
-      <h1>Tables DummyTable</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Header 1</th>
+            <th>Header 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
